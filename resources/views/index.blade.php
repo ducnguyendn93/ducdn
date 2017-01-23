95572b23e68b6890af1e78853175a8f0ba27e08f
QuangDuc <nguyenquangducdn93@gmail.com>
2017-01-23 18:49:26 +0700

    resource

---
+++ resources/views/index.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>lập trình laravel</title>
+	
+	<link rel="stylesheet" href="assets/css/help.css">
+	
+</head>
+<body>
+<div id ="wrapper">
+<div id ="menu">
+
+<img src="/image/h2.jpg" class="img-responsive" alt=""  />
+
+<a href="" class="peremium" target="">Premium</a>
+<a href="" class="help" target="">Help</a>
+<a href="" class="down" target="">Download</a>  
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="" class="signup" target="">Sign Up </a>  
+<a href="" class="login" target="">Login </a>  
+
+
+
+
+</div>
+<div id ="header">
+	<img src="/image/background.jpg" class="img-background" alt="" /> 
+	<div><h3 class="text">How can we help you?</h3> </div>
+	<input type="text" name="search" placeholder="Search..">
+	<div><h4 class="text2">SOLUTIONS</h4> </div>
+	<div><h4 class="text3">PREMIUM FOR FAMILY</h4> </div>
+	<div><h4 class="text4">STUDENT DISCOUNT</h4> </div>
+	<div><h4 class="text5">SLUTIONS</h4> </div>
+
+
+<div><a href=""><h3 class="question1">Problems loggin in</h3></a>  </div>
+	<div><a href=""><h3 class="question2">Premium for family FAQ</h3></a> </div>
+	<div><a href=""><h3 class="question3">Student discount FAQ</h3></a> </div>
+	<div><a href=""><h3 class="question4">Why am I being charget every month?</h3></a>
+
+ </div>
+</div>
+
+<div id ="content">
+<div class="control">
+
+<ul>
+ <li><a href="#">Account& Payment</a></li>
+     <li><a href="#">Listen Everywhere</a></li>
+     <li><a href="#">Using Spotify</a></li>
+     </ul>
+</div>
+<div class="display">
+<ul>
+ <li><a href="#">Student Discount</a></li>
+     <li><a href="#">Premium for Family</a></li>
+     <li><a href="#">Subscription Options</a></li>
+     <li><a href="#">Solutions</a></li>
+     <li><a href="#">Managing Payments</a></li>
+     </ul>
+	
+</div>
+<div class="display2">
+<ul>
+     <li><a href="#">One phone,tablet&Desktop</a></li>
+     <li><a href="#">Supported connection types</a></li>
+     <li><a href="#">One other devices</a></li>
+     <li><a href="#">One Play Stations</a></li>
+     <li><a href="#">One Speakers</a></li>
+ </ul>
+	
+</div>
+<div class="display3">
+<ul>
+ <li><a href="#">Playlists</a></li>
+     <li><a href="#">Search&Play</a></li>
+     <li><a href="#">Troubleshooting</a></li>
+     <li><a href="#">For Artits</a></li>
+     <li><a href="#">Share Music</a></li>
+     </ul>
+	
+</div>
+<div class="show">
+   <ul>
+     <li><a href="#">Show All</a></li>
+     <li><a href="#">Show All</a></li>
+     <li><a href="#">Show All</a></li>
+   </ul>
+</div>
+	<div><h3 class="text6">Ask the Community</h3> </div>
+
+	<p class="text7"> Have Questions? Our worldwide Community of expert fans can answer them.Have answers?Join the Community and help!</p>
+	<div class="text8">
+<p>Switch to Student Discount</p>
+<p>I Have full internet connection,but it says I'm offline?</p>
+<p>Charged twice per month</p>
+</div>
+<div>
+<a href="#" class="button">ASK THE COMMUNITY</a>
+</div>
+</div>
+
+<div id ="about">
+	<p class="text9">How to use the features</p>	
+     <p class="about1">GET STARTED</p>
+     <p class="about2">AND CHECK THESE OUT</p>
+     <h3 class="">Search</h3>
+
+     <h3 class="">Radio</h3>
+     <p class="text10">Search a song, album, artist, label, genre, mood, activity, or friend on Spotify.</p>
+     <p class="text11">Tune into one of our many stations or create one based on your favorite artist, album, or song.</p>
+    
+     <a href="" class="text12">READ MORE</a>
+     <a href="" class="text13">READ MORE</a>
+     <h3 class="">Browse</h3>
+     <h3 class="">Discover Weekly</h3>
+     <p class="text14">Browse new and top-ranked music. Find playlists to fit - or change - your mood.</p>
+     <p class="text15">Wonder how you'll find new music? Every week Spotify builds a playlist for you of tracks based on your taste but that you may have never heard before!</p>	
+      <a href="" class="text16">READ MORE</a>
+      <a href="" class="text17">READ MORE</a>
+
+       <h3 class="">Playlist</h3>
+     <h3 class="">Spotify on Chormecast</h3>
+     <p class="text18">Playlists are like mixtapes you create on Spotify. You can share them, subscribe to them, and collaborate on them with your friends.</p>
+     <p class="text19">Play Spotify on your other favorite service: Chromecast.</p>
+
+       <a href="" class="text20">READ MORE</a>
+      <a href="" class="text21">READ MORE</a>
+    
+
+     </div>
+   
+
+<div id ="contact">
+  	<h3 class="text22">Still not on Spotify?</h3>
+  	<div>
+  	<p class="text23">Over 30 million tracks; thousands of curated playlists; Discover Weekly; Spotify Running; Radio; Chromecast, sound system, car, TV, and PlayStation integration; sharing and creating playlists with your friends... All of this is ready for you.</p>
+  	</div>
+	 <a href="#" class="button2">GET SPOTIFY</a>
+</div>
+
+<div id ="footer">
+	<img src="/image/h4.png" class="img-responsive2" alt=""  />
+	 <p class="text24">COMPANY</p>
+  <P class="text25">COMMUNITIES</P>
+  <P class="text26">USEFUL LINKS</P>
+  
+  
+  <div class="menufooter">
+  <ul>
+    <li><a href=""><h3 class="">About</h3></a>  </li>
+	<li><a href=""><h3 class="">Jobs</h3></a> </li>
+	<li><a href=""><h3 class="">Press</h3></a> </li>
+	<li><a href=""><h3 class="">News</h3></a></li>
+	</ul>
+	</div>
+
+	 <div class="menufooter2">
+  <ul>
+    <li><a href=""><h3 class="">Artists</h3></a>  </li>
+	<li><a href=""><h3 class="">Developers</h3></a> </li>
+	<li><a href=""><h3 class="">Brands</h3></a> </li>
+	</ul>
+	</div>
+	 <div class="menufooter3">
+  <ul>
+    <li><a href=""><h3 class="">Help</h3></a>  </li>
+	<li><a href=""><h3 class="">Gift</h3></a> </li>
+	<li><a href=""><h3 class="">Web Player</h3></a> </li>
+	<li><a href=""><h3 class="">Contact Spotify</h3></a> </li>
+	</ul>
+	</div>
+	<div>
+	<img src="/image/icon05.png" class="hicon5" alt=""  />
+
+	</div>
+</div>
+ 
+
+
+	
+
+</body>
+</html>
